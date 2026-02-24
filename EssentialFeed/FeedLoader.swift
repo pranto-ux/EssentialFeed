91fe23068f53af5256ee3fa172123c8a9264b195
pranto <pranto@bitmorpher.com>
2026-02-24 15:48:49 +0600

    Group Fee Feature files

---
+++ EssentialFeed/FeedLoader.swift
@@ -0,0 +1,17 @@
+//
+//  FeedLoader.swift
+//  
+//
+//  Created by Photolab AI2 on 2/24/26.
+//
+
+import Foundation
+
+enum LoadFeedResult {
+    case success([FeedItem])
+    case error(Error)
+}
+
+protocol FeedLoader {
+    func load(completion: @escaping (LoadFeedResult) -> Void)
+}
